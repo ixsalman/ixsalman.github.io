14abefd2a7c4a47bd55f76b17ec380c49e240777
Allen Zhao <allenxyz@umich.edu>
2014-07-13 17:31:57 +0200

    Final version - hopefuly fixed

--- javascript/app.js
+++ javascript/app.js
@@ -11,7 +11,6 @@ function toggleHeader() {
 function click_div() {
   $(document).ready( function(event) {
     var name = window.location.href.substring(window.location.href.indexOf('#') + 1);
-    console.log(name);
     $("div").find("."+name).trigger("click");
   });
 };
